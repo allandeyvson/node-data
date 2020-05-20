ad2d40b487cb2660d5731641433b943fbd8c1933
allan araujo <deyvson7@gmail.com>
2020-05-20 15:45:20 -0300

    disponibilizando aplicacao no heroku

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const ContextStrategy = require('./src/db/strategies/base/contextStrategy')
-const MongoDB = require('./src/db/strategies/mongoDB')
-const Postgres = require('./src/db/strategies/postgres')
-
-//const contextMongo  = new ContextStrategy(new MongoDB())
-//contextMongo.create()
-
-//const contextPostgres  = new ContextStrategy(new Postgres())
-//contextPostgres.create()
-
+const api = require('./src/api/api')
